513d160a9177cd6be2d69d31a0e20ca97d2c9b8b
Bapurao-jadhav <bapurao912@gmail.com>
2026-08-30 22:56:52 +0530

    Update booking ssyatem userdetail

--- src/main/java/com/example/booking/security/CustomUserDetailsService.java
+++ src/main/java/com/example/booking/security/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.example.booking.security;
 
 import com.example.booking.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
@@ -9,12 +10,23 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-	private final UserRepository repo;
 
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return repo.findByUsername(username)
-				.map(u -> User.builder().username(u.getUsername()).password(u.getPassword())
-						.authorities(new SimpleGrantedAuthority("ROLE_" + u.getRole().name())).build())
-				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
-	}
+    private final UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        return repo.findByUsername(username)
+                .map(u -> User.withUsername(u.getUsername())
+                        .password(u.getPassword())
+                        .authorities(
+                                new SimpleGrantedAuthority(
+                                        "ROLE_" + u.getRole().name()
+                                )
+                        )
+                        .build())
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+    }
 }
